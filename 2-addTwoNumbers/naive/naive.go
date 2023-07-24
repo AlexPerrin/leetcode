0d869944643a50d022a4fbc86a2c3d2817c0bdcd
Alex Perrin <16arp5@queensu.ca>
2023-07-25 00:28:22 +0200

    complete

--- 2-addTwoNumbers/naive/naive.go
+++ 2-addTwoNumbers/naive/naive.go
@@ -6,5 +6,24 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	return &ListNode{}
+	l3 := &ListNode{}
+	l3Curr := l3
+	for l1 != nil || l2 != nil {
+		if l1 != nil {
+			l3Curr.Val += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			l3Curr.Val += l2.Val
+			l2 = l2.Next
+		}
+		if l3Curr.Val >= 10 {
+			l3Curr.Val = l3Curr.Val % 10
+			l3Curr.Next = &ListNode{Val: 1}
+		} else {
+			l3Curr.Next = &ListNode{}
+		}
+		l3Curr = l3Curr.Next
+	}
+	return l3
 }
